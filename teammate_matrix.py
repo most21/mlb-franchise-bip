58be830a29b0d771b2ed69337d6c39d3d4b6b964
Matthew Ost <most1@jhu.edu>
2020-12-31 11:15:10 -0500

    added tqdm and cleaned up teammate matrix code

--- teammate_matrix.py
+++ teammate_matrix.py
@@ -2,9 +2,24 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from tqdm import tqdm
+
 def build_teammate_matrix(save=False):
+    """
+    Construct adjacency matrix for teammates.
+    If A_ij = 1, player i and player j were teammates at one point. Else 0.
+    Note that because the "teammate" relationship is symmetric, we only compute the upper triangle of this matrix and fill in the lower entries simultaneously.
+
+    Args:
+        save: boolean value for whether to save the output of this function to a file
+
+    Returns:
+        Numpy 2d-array and dictionary mapping a player's index to his Fangraphs ID
+    """
+    # Load player list
     players = pd.read_csv("./data/players.csv")
     N = players.shape[0]
+    player_id_dict = {i: str(players["playerid"][i]) for i in range(N)}
 
     # Read all player data files into memory
     print("Loading data files...")
@@ -15,20 +30,19 @@ def build_teammate_matrix(save=False):
     # Create teammate matrix
     print("Building matrix...")
     matrix = np.zeros((N, N))
-    for i in range(N):
+    for i in tqdm(range(N)):
         # Get data for player i. We open it here for efficiency, since it can be reused
         df1 = data[str(players["playerid"][i])]
-        p1_data = set([(df1["ateam"][k], df1["aseason"][k]) for k in range(df1.shape[0])])
+        p1_data = set([(df1["ateam"][k], df1["aseason"][k]) for k in range(df1.shape[0])]) # turn dataframe into set of tuples (1 tuple per row)
 
         for j in range(i, N):
-            print(i, j)
             # If i and j are the same, just leave those entries as 0
             if i == j:
                 continue
 
             # Get data for player j
             df2 = data[str(players["playerid"][j])]
-            p2_data = set([(df2["ateam"][k], df2["aseason"][k]) for k in range(df2.shape[0])])
+            p2_data = set([(df2["ateam"][k], df2["aseason"][k]) for k in range(df2.shape[0])]) # turn dataframe into set of tuples (1 tuple per row)
 
             # Take intersection of player data. If the result is non-empty, these players were teammates at some point
             join = p1_data.intersection(p2_data)
@@ -36,18 +50,21 @@ def build_teammate_matrix(save=False):
                 matrix[i][j] = 1
                 matrix[j][i] = 1
 
-    # Pickle the teammate matrix so we don't have to recompute it every time
+    # Pickle the teammate matrix and id dict so we don't have to recompute it every time
     if save:
         print("Saving teammate matrix to file.")
-        with open("teammate_matrix2.b", "wb") as f:
-            pickle.dump(matrix, f)
+        with open("teammate_matrix.b", "wb") as f:
+            pickle.dump((matrix, player_id_dict), f)
 
-    return matrix
+    return matrix, player_id_dict
 
 def load_teammate_matrix(file):
     return pickle.load(open(file, "rb"))
 
 
 if __name__ == "__main__":
-    matrix = build_teammate_matrix(save=True)
+    #matrix, player_id_dict = build_teammate_matrix(save=True)
+    #print(matrix)
+    matrix, player_id_dict = load_teammate_matrix("teammate_matrix.b")
     print(matrix)
+    print(player_id_dict)
