0dd8b0b67d1457ab05ff106e5f6700be28f47d7a
Matthew Ost <most1@jhu.edu>
2021-01-04 12:49:27 -0500

    first attempt at implementing the optimization problem

---
+++ bip.py
@@ -0,0 +1,66 @@
+import mip
+import pandas as pd
+import sys
+
+from teammate_matrix import get_teammate_matrix
+
+def make_bip(players, idx_to_id_dict, teammate_matrix):
+    N = players.shape[0]
+
+    # Create model
+    m = mip.Model(sense=mip.MAXIMIZE, solver_name=mip.CBC)
+    # m.verbose = 0
+
+    # Create variables
+    x = [m.add_var(var_type=mip.BINARY, name='var({})'.format(i)) for i in range(N)]
+
+    # Create objective function
+    m.objective = mip.xsum(players["WAR"][i] * x[i] for i in range(N))
+
+    # Create first constraint: exactly 5 pitchers must be chosen
+    m += mip.xsum(x[i] for i in range(N)) == 5
+
+    # Create second set of constraints: no teammates allowed
+    for i in range(N):
+        m += mip.xsum(teammate_matrix.get(idx_to_id_dict[i], idx_to_id_dict[j], 0) * x[j] for j in range(N))
+
+    # Solve
+    m.optimize()
+
+    # Parse solution
+    for i in range(N):
+        if np.isclose(x[i][j].x, 1):
+            print(i, idx_to_id_dict[i], x[i][j].x, players["WAR"][i])
+
+    print(f"\nObjective function: {m.objective_value}")
+
+
+def load_players(team):
+    """
+    Read WAR rankings from file for the provided franchise.
+
+    Args:
+        team: string containing the franchise whose data should be loaded
+
+    Returns:
+        Dictionary mapping each player ID for the franchise to their WAR total
+    """
+    players = pd.read_csv(f"./data/franchise/{team}.csv")[["playerid", "WAR"]]
+    players["playerid"] = players["playerid"].astype("str")
+    return players, {i: players["playerid"][i] for i in range(players.shape[0])}
+
+def main():
+    # Command line args
+    team = sys.argv[1]
+
+    # Load data
+    players, idx_to_id_dict = load_players(team)
+    teammate_matrix = get_teammate_matrix(matrix_loc="teammate_matrix.b")
+
+    # Create and solve optimization problem
+    make_bip(players, idx_to_id_dict, teammate_matrix)
+
+
+
+if __name__ == "__main__":
+    main()
--- teammate_matrix.py
+++ teammate_matrix.py
@@ -1,25 +1,28 @@
 import numpy as np
+import os
 import pandas as pd
 import pickle
 
 from tqdm import tqdm
 
-def build_teammate_matrix(save=False):
+def build_teammate_matrix(save=True):
     """
-    Construct adjacency matrix for teammates.
-    If A_ij = 1, player i and player j were teammates at one point. Else 0.
+    Construct adjacency matrix for teammates. Stored as dictionary for efficiency.
+    If A_ij = 1, player i and player j were teammates at one point. Else 0. We only store the entries with value 1.
     Note that because the "teammate" relationship is symmetric, we only compute the upper triangle of this matrix and fill in the lower entries simultaneously.
 
     Args:
         save: boolean value for whether to save the output of this function to a file
 
     Returns:
-        Numpy 2d-array and dictionary mapping a player's index to his Fangraphs ID
+        Dictionary mapping tuple of every 2 players' IDs to the matrix value
+
+    # Numpy 2d-array and dictionary mapping a player's index to his Fangraphs ID
     """
     # Load player list
     players = pd.read_csv("./data/players.csv")
     N = players.shape[0]
-    player_id_dict = {i: str(players["playerid"][i]) for i in range(N)}
+    #player_id_dict = {i: str(players["playerid"][i]) for i in range(N)}
 
     # Read all player data files into memory
     print("Loading data files...")
@@ -29,10 +32,11 @@ def build_teammate_matrix(save=False):
 
     # Create teammate matrix
     print("Building matrix...")
-    matrix = np.zeros((N, N))
+    matrix = {} #np.zeros((N, N))
     for i in tqdm(range(N)):
         # Get data for player i. We open it here for efficiency, since it can be reused
-        df1 = data[str(players["playerid"][i])]
+        pid1 = str(players["playerid"][i])
+        df1 = data[pid1]
         p1_data = set([(df1["ateam"][k], df1["aseason"][k]) for k in range(df1.shape[0])]) # turn dataframe into set of tuples (1 tuple per row)
 
         for j in range(i, N):
@@ -41,30 +45,48 @@ def build_teammate_matrix(save=False):
                 continue
 
             # Get data for player j
-            df2 = data[str(players["playerid"][j])]
+            pid2 = str(players["playerid"][j])
+            df2 = data[pid2]
             p2_data = set([(df2["ateam"][k], df2["aseason"][k]) for k in range(df2.shape[0])]) # turn dataframe into set of tuples (1 tuple per row)
 
             # Take intersection of player data. If the result is non-empty, these players were teammates at some point
             join = p1_data.intersection(p2_data)
             if len(join) > 0:
-                matrix[i][j] = 1
-                matrix[j][i] = 1
+                matrix[pid1, pid2] = 1
+                matrix[pid2, pid1] = 1
 
-    # Pickle the teammate matrix and id dict so we don't have to recompute it every time
+    # Pickle the teammate "matrix" and id dict so we don't have to recompute it every time
     if save:
         print("Saving teammate matrix to file.")
         with open("teammate_matrix.b", "wb") as f:
-            pickle.dump((matrix, player_id_dict), f)
+            #pickle.dump((matrix, player_id_dict), f)
+            pickle.dump(matrix, f)
 
-    return matrix, player_id_dict
+    return matrix #, player_id_dict
 
 def load_teammate_matrix(file):
     return pickle.load(open(file, "rb"))
 
+def get_teammate_matrix(matrix_loc=None, **kwargs):
+    """
+    Wrapper function that loads the teammate matrix from the provided location or creates a new one if necessary.
+
+    Args:
+        matrix_loc: path to the existing pickled matrix file. If None or invalid path, a new file will be created and saved.
+
+    Returns:
+        Dictionary mapping tuple of every 2 players' IDs to the matrix value
+
+        #Numpy 2d-array and dictionary mapping a player's index to his Fangraphs ID
+    """
+    if matrix_loc is not None and os.path.exists(matrix_loc):
+        print(f"Loading teammate matrix from {matrix_loc}")
+        return load_teammate_matrix(matrix_loc)
+    else:
+        print("Creating teammate matrix from scratch")
+        return build_teammate_matrix(**kwargs)
 
 if __name__ == "__main__":
-    #matrix, player_id_dict = build_teammate_matrix(save=True)
+    #matrix = build_teammate_matrix(save=True)
     #print(matrix)
-    matrix, player_id_dict = load_teammate_matrix("teammate_matrix.b")
-    print(matrix)
-    print(player_id_dict)
+    pass
